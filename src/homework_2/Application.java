1d216b19754c397b99b5a76e56af99033d60a7f1
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-01-28 18:18:02 +0400

    Finished and added homework 1, 2

---
+++ src/homework_2/Application.java
@@ -0,0 +1,42 @@
+package homework_2;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    static int method1(int a, int b) {
+        return 0;
+    }
+
+    static void method2() {
+
+    }
+
+    static void method3(boolean a) {
+        System.out.println("Hello World");
+    }
+
+    static char method4(char a) {
+        return a;
+    }
+
+    static float method5(float a, float b) {
+        return a;
+    }
+
+    static void method6() {
+        method6();
+    }
+
+    /**
+     * @param a an integer number
+     * @param b an integer number
+     * @return sum of a, b
+     */
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
