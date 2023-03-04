0ce2764229a968fc1e1691567f887ccfdd67bd0a
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-04 21:41:58 +0400

    Added [seasons] package in h10_enum

---
+++ src/h10_enum/seasons/Seasons.java
@@ -0,0 +1,32 @@
+package h10_enum.seasons;
+
+public enum Seasons {
+    SPRING(10, 25),
+    SUMMER(31, 45),
+    FALL(26, 30),
+    WINTER(-30, 9);
+
+    private int minTemperature;
+    private int maxTemperature;
+
+    Seasons(int minTemperature, int maxTemperature) {
+        this.minTemperature = minTemperature;
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+}
---
+++ src/h10_enum/seasons/Test.java
@@ -0,0 +1,8 @@
+package h10_enum.seasons;
+
+public class Test {
+    public static void main(String[] args) {
+        SeasonsService service = new SeasonsService();
+        System.out.println(service.getSeason(100));
+    }
+}
