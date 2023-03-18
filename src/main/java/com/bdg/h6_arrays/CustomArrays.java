6ddb126ca073c78ee5e652c67710878daf13a19c
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-19 02:49:42 +0400

    Optimized sumOfPrincipalDiagonal() method in main/java/com/bdg/h6_arrays/CustomArrays.java

--- src/main/java/com/bdg/h6_arrays/CustomArrays.java
+++ src/main/java/com/bdg/h6_arrays/CustomArrays.java
@@ -536,11 +536,7 @@ public class CustomArrays {
 
         int sum = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i == j) {
-                    sum += matrix[i][i];
-                }
-            }
+            sum += matrix[i][i];
         }
         return sum;
     }
