a293f9e204f2793c54214ee2f1ea6a4ae5da943c
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-19 03:08:29 +0400

    Optimized sumOfSecondaryDiagonal() method in main/java/com/bdg/h6_arrays/CustomArrays.java

--- src/main/java/com/bdg/h6_arrays/CustomArrays.java
+++ src/main/java/com/bdg/h6_arrays/CustomArrays.java
@@ -557,11 +557,7 @@ public class CustomArrays {
 
         int sum = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i + j == matrix.length - 1) {
-                    sum += matrix[i][j];
-                }
-            }
+            sum += matrix[i][matrix.length - (i + 1)];
         }
         return sum;
     }
