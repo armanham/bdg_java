cbe59bfc541452151ae8fe6d58ebf14d8f0ddfab
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-19 03:11:49 +0400

    Optimized invertMatrixToTheMainDiagonal() method in main/java/com/bdg/h6_arrays/CustomArrays.java

--- src/main/java/com/bdg/h6_arrays/CustomArrays.java
+++ src/main/java/com/bdg/h6_arrays/CustomArrays.java
@@ -475,11 +475,10 @@ public class CustomArrays {
             return matrix;
         }
 
-        int temp;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (j > i) {
-                    temp = matrix[i][j];
+                    int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
                 }
