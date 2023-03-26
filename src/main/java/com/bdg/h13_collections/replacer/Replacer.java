0dcd91924189a0acfd162855af8c7a7ab45d7a07
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-26 23:03:49 +0400

    Added javadoc for replaceAll() method in Replacer.java file in main/java/com/bdg/h13_collections/replacer/

--- src/main/java/com/bdg/h13_collections/replacer/Replacer.java
+++ src/main/java/com/bdg/h13_collections/replacer/Replacer.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class Replacer<T> {
 
+    /**
+     * This method replaces elements of specified list with new specified element.
+     *
+     * @param list        ArrayList<T> - type.
+     * @param oldElements T - type.
+     * @param newElement  T - type.
+     * @throws IllegalArgumentException if passed list is null or empty.
+     */
     public void replaceAll(ArrayList<T> list, T oldElements, T newElement) throws IllegalArgumentException {
         if (list == null || list.isEmpty()) {
             throw new IllegalArgumentException("Passed null or empty value: ");
