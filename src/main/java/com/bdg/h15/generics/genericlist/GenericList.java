ed31c322c5bf3505e7ac1f5958bf315657e5b8d5
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-03-31 19:55:53 +0400

    Added genericlist/ in main/java/com/bdg/h15/generics/

---
+++ src/main/java/com/bdg/h15/generics/genericlist/GenericList.java
@@ -0,0 +1,221 @@
+package com.bdg.h15.generics.genericlist;
+
+public class GenericList<E> {
+
+    private Object[] elements;
+    private int size;
+    private final static int DEFAULT_CAPACITY = 10;
+    private int capacity;
+
+    public GenericList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be in range [1; Integer.MAX_VALUE]");
+        }
+        this.capacity = capacity;
+        elements = new Object[capacity];
+    }
+
+    public GenericList(E... elements) {
+        this.size = elements.length;
+        this.capacity = size;
+        this.elements = new Object[size];
+
+        for (int i = 0; i < elements.length; i++) {
+            this.elements[i] = elements[i];
+        }
+    }
+
+    public GenericList() {
+        capacity = DEFAULT_CAPACITY;
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public boolean contains(Object o) {
+        if (isEmpty()) {
+            return false;
+        }
+
+        if (o == null) {
+            for (Object item : elements) {
+                if (item == null) {
+                    return true;
+                }
+            }
+        }
+
+        for (Object item : elements) {
+            if (item.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean add(E e) {
+        if (size == capacity) {
+            ensureCapacity();
+        }
+        Object[] copy = copyOfCurrentElementsToCapacityEnsuredArray();
+        size = size + 1;
+        copy[size - 1] = e;
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        int firstIndexOfO = indexOf(o);
+
+        if (firstIndexOfO == -1) {
+            return false;
+        }
+
+        elements = shiftLeftFrom(firstIndexOfO);
+        trimToSize();
+        return true;
+    }
+
+    public void clear() {
+        size = 0;
+        trimToSize();
+    }
+
+    public E get(int index) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+        return (E) elements[index];
+    }
+
+    public E set(int index, E element) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+
+        elements[index] = element;
+        return element;
+    }
+
+    public void add(int index, E element) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+
+        if (size == capacity) {
+            ensureCapacity();
+        }
+
+        elements = shiftRightFrom(index);
+        elements[index] = element;
+    }
+
+    public E remove(int index) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+
+        E removedElement = get(index);
+        elements = shiftLeftFrom(index);
+        trimToSize();
+        return removedElement;
+    }
+
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void ensureCapacity() {
+        capacity = capacity * 2;
+    }
+
+    private void trimToSize() {
+        capacity = size;
+    }
+
+    private Object[] copyOfCurrentElementsToCapacityEnsuredArray() {
+        Object[] copy = new Object[capacity];
+        for (int i = 0; i < elements.length; i++) {
+            copy[i] = elements[i];
+        }
+        elements = copy;
+        return elements;
+    }
+
+
+    private Object[] shiftLeftFrom(int index) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+
+        Object[] copy = elements;
+        for (int i = index; i < size; i++) {
+            if (i != size - 1) {
+                copy[i] = elements[i + 1];
+            } else {
+                copy[i] = 0;
+            }
+        }
+        size = size - 1;
+        return copy;
+    }
+
+    private Object[] shiftRightFrom(int index) {
+        if (!isInRange(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds: ");
+        }
+
+        Object[] copy = new Object[size + 1];
+        for (int i = 0; i < size; i++) {
+            copy[i] = elements[i];
+        }
+
+        for (int i = index; i < size + 1; i++) {
+            if (i <= size - 1) {
+                copy[i + 1] = elements[i];
+            }
+        }
+        copy[copy.length - 1] = elements[size - 1];
+        size = size + 1;
+        return copy;
+    }
+
+    private boolean isInRange(int index) {
+        return index >= 0 && index < size;
+    }
+}
